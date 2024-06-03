b56eeb517c08097c0087b799145fc624237d4990
Christian Cueni <christian.cueni@iterativ.ch>
2024-06-03 20:46:45 +0200

    Refactor Account tab

--- components/Accounts.vue
+++ components/Accounts.vue
@@ -1,27 +1,43 @@
 <script setup lang="ts">
-import type { Month, Transaction } from "./types/index";
+import type { Account } from "./types/index";
 import Button from "primevue/button";
 
 const props = defineProps<{
-  title: string;
-  month: Month;
-  transactions: Transaction[];
+  accounts: Account[];
 }>();
 
 const emit = defineEmits<{
-  (e: "addTransaction", transaction: Transaction): void;
+  (e: "addAccount", account: Account): void;
 }>();
 
 const visible = ref(false);
 const selectedAccountType: Ref<AccountType> = ref("cash");
+const accountName = ref("");
 const accountTypes: { type: AccountType; label: string }[] = [
   { type: "cash", label: "Cash" },
   { type: "investment", label: "Investment" },
   { type: "pension", label: "Pension" },
 ];
+
+function addAccount() {
+  emit("addAccount", accountName.value, selectedAccountType.value);
+  selectedAccountType.value = accountTypes[0];
+  accountName.value = "";
+  visible.value = false;
+}
 </script>
 
 <template>
+  <div v-for="account in props.accounts" :key="account.name">
+    <div class="m-0 grid grid-cols-13 gap-4">
+      account {{ account.name }}
+      <p>Account type: {{ account.type }}</p>
+      <p v-for="balance in account.balances">
+        {{ balance }}
+      </p>
+    </div>
+  </div>
+  <Button label="Add account" @click="visible = true" />
   <Dialog
     v-model:visible="visible"
     modal
@@ -29,14 +45,19 @@ const accountTypes: { type: AccountType; label: string }[] = [
     :style="{ width: '25rem' }"
   >
     <span class="text-surface-600 dark:text-surface-0/70 block mb-5"
-      >Add account</span
+      >Update your information.</span
     >
     <div class="flex items-center gap-3 mb-3">
-      <label for="username" class="font-semibold w-[6rem]">Name</label>
-      <InputText id="username" class="flex-auto" autocomplete="off" />
+      <label for="name" class="font-semibold w-[6rem]">Name</label>
+      <InputText
+        id="name"
+        class="flex-auto"
+        autocomplete="off"
+        v-model="accountName"
+      />
     </div>
     <div class="flex items-center gap-3 mb-5">
-      <label for="email" class="font-semibold w-[6rem]">Account Type</label>
+      <label for="label" class="font-semibold w-[6rem]">Account Type</label>
       <Dropdown
         v-model="selectedAccountType"
         :options="accountTypes"
--- pages/index.vue
+++ pages/index.vue
@@ -9,24 +9,8 @@ import { months } from "../utils/index";
 const transactionStore = useTransactionStore();
 const accountStore = useAccountStore();
 
-const visible = ref(false);
-const selectedAccountType: Ref<AccountType> = ref("cash");
-const accountName = ref("");
-const accountTypes: { type: AccountType; label: string }[] = [
-  { type: "cash", label: "Cash" },
-  { type: "investment", label: "Investment" },
-  { type: "pension", label: "Pension" },
-];
-
 function calculateMonthlyTotal(transactions: Transaction[]) {
   return transactions.reduce((acc, transaction) => acc + transaction.amount, 0);
-}
-
-function addAccount() {
-  accountStore.addAccount(accountName.value, selectedAccountType.value.type);
-  selectedAccountType.value = accountTypes[0];
-  accountName.value = "";
-  visible.value = false;
 }
 </script>
 <template>
@@ -70,55 +54,11 @@ function addAccount() {
         <p>Sum: {{ transactionStore.yearlyExpenditure }}</p>
       </AccordionTab>
       <AccordionTab header="Accounts">
-        <div v-for="account in accountStore.accounts" :key="account.name">
-          <div class="m-0 grid grid-cols-13 gap-4">
-            account {{ account.name }}
-            <p>Account type: {{ account.type }}</p>
-            <p v-for="balance in account.balances">
-              {{ balance }}
-            </p>
-          </div>
-        </div>
-        <Button label="Add account" @click="visible = true" />
+        <Accounts
+          :accounts="accountStore.accounts"
+          @add-account="accountStore.addAccount"
+        />
       </AccordionTab>
     </Accordion>
   </div>
-  <Dialog
-    v-model:visible="visible"
-    modal
-    header="Add account"
-    :style="{ width: '25rem' }"
-  >
-    <span class="text-surface-600 dark:text-surface-0/70 block mb-5"
-      >Update your information.</span
-    >
-    <div class="flex items-center gap-3 mb-3">
-      <label for="name" class="font-semibold w-[6rem]">Name</label>
-      <InputText
-        id="name"
-        class="flex-auto"
-        autocomplete="off"
-        v-model="accountName"
-      />
-    </div>
-    <div class="flex items-center gap-3 mb-5">
-      <label for="label" class="font-semibold w-[6rem]">Account Type</label>
-      <Dropdown
-        v-model="selectedAccountType"
-        :options="accountTypes"
-        optionLabel="label"
-        placeholder="Select an account type"
-        class="w-full md:w-[14rem]"
-      />
-    </div>
-    <div class="flex justify-end gap-2">
-      <Button
-        type="button"
-        label="Cancel"
-        severity="secondary"
-        @click="visible = false"
-      ></Button>
-      <Button type="button" label="Save" @click="addAccount()"></Button>
-    </div>
-  </Dialog>
 </template>
